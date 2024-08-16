0c3937384b5760a6769aa9f224a57eaa60d09a5c
Yehani Athukorala <yehaniathukorala2001@gmail.com>
2024-08-16 18:30:22 +0530

    Delete lecspace-Backend-Room Managemnet &Booking Process/lecspace-Backend-main directory

--- lecspace-Backend-Room Managemnet &Booking Process/lecspace-Backend-main/src/main/java/com/ictproject_thirdyear/Lecspace/repository/RoomRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ictproject_thirdyear.Lecspace.repository;
-import com.ictproject_thirdyear.Lecspace.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room,Long>{}
